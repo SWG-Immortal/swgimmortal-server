966e80229a743e62b7a042144a30c3f64befc170
Kevin Schmidt <kevinschmidt777@hotmail.de>
2024-12-31 16:46:45 +0100

    decreased newb helper buffs

--- MMOCoreORB/bin/scripts/managers/player_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_manager.lua
@@ -48,8 +48,8 @@ onlineCharactersPerAccount = 2 --How many characters are allowed online from a s
 allowSameAccountPvpRatingCredit = 0
 
 --Blue frog / GM buff values
-performanceBuff = 1100
-medicalBuff = 2200
+performanceBuff = 500
+medicalBuff = 1800
 performanceDuration = 7200 -- in seconds
 medicalDuration = 7200 -- in seconds
 
