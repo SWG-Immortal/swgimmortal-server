87900e500ef62d226c1e7e0cf143b2f553352fd1
Kevin Schmidt <kevinschmidt777@hotmail.de>
2025-01-06 15:41:19 +0100

    auto-join GALAXY chat on character creation.

--- MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
+++ MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
@@ -563,6 +563,9 @@ bool PlayerCreationManager::createCharacter(ClientCreateCharacterCallback* callb
 	//Join auction chat room
 	ghost->addChatRoom(chatManager->getAuctionRoom()->getRoomID());
 
+	// SWG-Immortal: Auto-join GALAXY room on character creation.
+	ghost->addChatRoom(chatManager->getGalaxyRoom()->getRoomID());
+
 	ManagedReference<SuiMessageBox*> box = new SuiMessageBox(playerCreature, SuiWindowType::NONE);
 	box->setPromptTitle("PLEASE NOTE");
 	box->setPromptText("You are limited to creating one character per hour. Attempting to create another character or deleting your character before the 1 hour timer expires will reset the timer.");
